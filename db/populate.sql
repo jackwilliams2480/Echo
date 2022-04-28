2a4f7213217f01e834e72b4575640acbe9d66f6c
Jack <jackwilliams2480@gmail.com>
2022-04-28 00:13:02 -0400

    update db files

--- db/populate.sql
+++ db/populate.sql
@@ -7,4 +7,6 @@ TABLE "music" (
   "location" varchar
 )
 */
-INSERT INTO music VALUES(1,'Travis Scott', 'Love Sick', 'Rap', '../server/audio_files/rap/travis_scott/');
+INSERT INTO music VALUES(1,'Travis Scott', 'Drugs You Should Try It', 'Rap', 'server/audio_files/rap/travis_scott/Drugs You Should Try It.mp3');
+INSERT INTO music VALUES(1,'Travis Scott', 'Love Sick', 'Rap', 'server/audio_files/rap/travis_scott/LIGHTS (LOVE SICK).mp3');
+INSERT INTO music VALUES(1,'Travis Scott', 'MAFIA', 'Rap', 'server/audio_files/rap/travis_scott/MAFIA.mp3');
--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,22 @@
-CREATE TABLE "users" (
-  "id" int PRIMARY KEY,
-  "email" varchar,
-  "password" varchar,
-  "username" varchar
+CREATE TABLE users (
+  id int PRIMARY KEY,
+  email varchar,
+  password varchar,
+  username varchar
 );
 
-CREATE TABLE "music" (
-  "id" int PRIMARY KEY,
-  "artist" varchar,
-  "title" varchar,
-  "genre" varchar,
-  "location" varchar
+CREATE TABLE music (
+  id int PRIMARY KEY,
+  artist varchar,
+  title varchar,
+  genre varchar,
+  location varchar
 );
 
-CREATE TABLE "playlist" (
-  "userid" int PRIMARY KEY,
-  "musicid" int
+CREATE TABLE playlist (
+  userid int PRIMARY KEY,
+  musicid int
 );
 
-ALTER TABLE "users" ADD FOREIGN KEY ("id") REFERENCES "playlist" ("userid");
-
-ALTER TABLE "playlist" ADD FOREIGN KEY ("musicid") REFERENCES "music" ("id");
+ALTER TABLE playlist ADD FOREIGN KEY (userid) REFERENCES users (id);
+ALTER TABLE playlist ADD FOREIGN KEY (musicid) REFERENCES music (id);
